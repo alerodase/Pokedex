914e2d8610b58f1a31c95566db14bbda8488ab23
alerodase <alerodase@gmail.com>
2023-02-14 22:08:13 +0100

    actualizado 14/02/2023

--- pokedex.js
+++ pokedex.js
@@ -1,33 +1,31 @@
 const main = document.getElementById("main");
-const inputNombre=document.getElementById('nombre')
-const inputTipo=document.getElementById('tipo')
-const select=document.getElementById('ordenar')
-
+const inputNombre = document.getElementById("nombre");
+// const inputTipo = document.getElementById("tipo");
+// const select = document.getElementById("ordenar");
 
 async function getAllPokemon() {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`);
   const data = await response.json();
-  return data
+  return data;
 }
 
-async function pintarTodosLosPokemon(){
-  const todosLosPokemon=await getAllPokemon()
-  todosLosPokemon.results.forEach(async pokemon=>{
-  await pintarUnPokemon(pokemon.name)
-  
-  })
-}
-inputNombre.addEventListener('keyup',async()=>{
-  main.innerHTML=''
-  const datos=await getAllPokemon()
-  const datosFiltrados=datos.results.filter(x=>x.name.includes(inputNombre.value))
-  datosFiltrados.forEach(async x => {
-    await pintarUnPokemon(x.name)
+async function pintarTodosLosPokemon() {
+  const todosLosPokemon = await getAllPokemon();
+  todosLosPokemon.results.forEach(async (pokemon) => {
+    await pintarUnPokemon(pokemon.name);
   });
-})
-
-
-
+}
+inputNombre.addEventListener("keyup", async () => {
+  main.innerHTML = "";
+  const datos = await getAllPokemon();
+  const datosFiltrados = datos.results.filter((x) =>
+    x.name.includes(inputNombre.value)
+  );
+  datosFiltrados.forEach(async (x) => {
+    await pintarUnPokemon(x.name);
+  });
+  
+});
 
 // inputNombre.addEventListener('keyup',async()=>{
 //   main.innerHTML=''
@@ -43,7 +41,6 @@ inputNombre.addEventListener('keyup',async()=>{
 
 console.log(getAllPokemon());
 
-
 async function getPokemonData(pokemonId) {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
@@ -53,44 +50,45 @@ async function getPokemonData(pokemonId) {
 }
 console.log(getPokemonData(1));
 
-async function pintarUnPokemon(pokemonId){
-  const pokemonData= await getPokemonData(pokemonId)
-  const divContenedor=document.createElement('div')
-  const divContenido2=document.createElement('div')
-  const h2=document.createElement('h2')
-  const divTipo=document.createElement('div')
-  const divContenido=document.createElement('div')
-  const divImg=document.createElement('div')
-  const pId=document.createElement('p')
-  
-  h2.innerHTML=pokemonData.name
-  divImg.innerHTML=`<img class='width' src="${pokemonData.sprites.front_default}"/>`
-  pId.innerHTML=pokemonData.id
+async function pintarUnPokemon(pokemonId) {
+  const pokemonData = await getPokemonData(pokemonId);
+  const divContenedor = document.createElement("div");
+  const divContenido2 = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const divTipo = document.createElement("div");
+  const divContenido = document.createElement("div");
+  const divImg = document.createElement("div");
+  const h2Id = document.createElement("h2");
+  divIPT=document.createElement('div')
 
-  divContenedor.className='b-main__div'
-  divContenido.className=`b-main__div__div-contenido`
-  divContenido2.className=pokemonData.types[0].type.name
-  divTipo.className='b-main__div__div-tipo'
-  pId.className='b-main__div__p'
-  h2.className='b-main__div__div-contenido__h2'
-  
- pokemonData.types.forEach(pokemon=>{
-  const pTipo=document.createElement('p')
-  pTipo.innerHTML=pokemon.type.name
-  divTipo.appendChild(pTipo)
-  pTipo.className='b-main__div__div-tipo__p'
- })
+  h2.innerHTML = pokemonData.name;
+  divImg.innerHTML = `<img class='width' src="${pokemonData.sprites.other.dream_world.front_default}"/>`;
+  h2Id.innerHTML = pokemonData.id;
 
-  main.appendChild(divContenedor)
-  divContenido2.appendChild(divContenido)
-  divContenido.appendChild(pId)
-  divContenido.appendChild(divImg)
-  divContenido.appendChild(h2)
-  divContenido.appendChild(divTipo)
-  divContenedor.appendChild(divContenido2)
+  divContenedor.className = "b-main__div";
+  divContenido.className = `b-main__div__div-contenido`;
+  divContenido2.className = pokemonData.types[0].type.name;
+  divTipo.className = "b-main__div__div-tipo";
+  h2Id.className = "b-main__div__p";
+  h2.className = "b-main__div__div-contenido__h2";
+  divImg.className='width_div'
+  divIPT.className='b-main__div__div-contenido__divIPT'
 
+  pokemonData.types.forEach((pokemon) => {
+    const pTipo = document.createElement("p");
+    pTipo.innerHTML = pokemon.type.name;
+    divTipo.appendChild(pTipo);
+    pTipo.className = "b-main__div__div-tipo__p";
+  });
 
+  main.appendChild(divContenedor);
+  divContenido2.appendChild(divContenido);
+  divContenido.appendChild(h2Id);
+  divIPT.appendChild(divImg);
+  divIPT.appendChild(h2);
+  divIPT.appendChild(divTipo);
+  divContenedor.appendChild(divContenido2);
+  divContenido.appendChild(divIPT)
 }
 
-
-pintarTodosLosPokemon()
+pintarTodosLosPokemon();
